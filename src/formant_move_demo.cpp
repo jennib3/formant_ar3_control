bf6b90a5ecf7c95cd3ee9986e1438c585e0c719c
jennib3 <briansaccount@gmail.com>
2021-07-19 10:15:19 -0600

    change over to rate control with some added features

--- src/formant_move_demo.cpp
+++ src/formant_move_demo.cpp
@@ -65,6 +65,7 @@ private:
   boost::mutex control_mutex;
   double arm_position[3];
   double arm_euler[3];
+  bool should_home{false};
 
 public:
 
@@ -179,10 +180,54 @@ public:
     return ret;
   }
 
+  void set_arm_rpy(int i, double value) {
+    control_mutex.lock();
+    arm_euler[i] = value;
+    control_mutex.unlock();
+  }
+  
+  void set_arm_pos(int i, double value) {
+    control_mutex.lock();
+    arm_position[i] = value;
+    control_mutex.unlock();
+  }
+
+  double get_arm_rpy(int i) {
+    double ret;
+    control_mutex.lock();
+    ret = arm_euler[i];
+    control_mutex.unlock();
+    return ret;
+  }
+
+  double get_arm_pos(int i) {
+    double ret;
+    control_mutex.lock();
+    ret = arm_position[i];
+    control_mutex.unlock();
+    return ret;
+  }
+
+
+  void set_home(bool value) {
+    control_mutex.lock();
+    should_home = value;
+    control_mutex.unlock();
+  }
+
+  bool get_home() {
+    control_mutex.lock();
+    bool ret = should_home;
+    control_mutex.unlock();
+    return ret;
+  }
+
 };
 
 sensor_msgs::JointState proposed_joint_state;
 Arm_control_class arm_control;
+Arm_control_class arm_planning;
+Arm_control_class new_arm_planning;
 
 
 bool plan_pose(moveit::planning_interface::MoveGroupInterface *move_group, geometry_msgs::Pose my_pose, ros::Publisher planned_joints_pub){
@@ -191,7 +236,7 @@ bool plan_pose(moveit::planning_interface::MoveGroupInterface *move_group, geome
   
   move_group->setPoseTarget(my_pose);
 
-  move_group->setPlanningTime(3.0);
+  move_group->setPlanningTime(0.3);
   
   bool success = (move_group->plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
   
@@ -234,6 +279,13 @@ bool plan_joints(moveit::planning_interface::MoveGroupInterface *move_group, std
 void plan_and_move(const std_msgs::Bool::ConstPtr& msg) {
   // msg->data;
   ROS_INFO_NAMED("msg", "plan_and_move");
+  arm_control.set_new_move(true);
+}
+
+void home(const std_msgs::Bool::ConstPtr& msg) {
+  // msg->data;
+  ROS_INFO_NAMED("msg", "home");
+  arm_control.set_home(true);
   arm_control.set_new_move(true);
 }
 
@@ -290,9 +342,13 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "formant_ar3_control_demo");
   ros::NodeHandle node_handle;
 
-  arm_control.set_arm_x(-0.007443);
-  arm_control.set_arm_y(-0.064925);
-  arm_control.set_arm_z(0.717915);
+  arm_planning.set_arm_x(-0.007443f);
+  arm_planning.set_arm_y(-0.064925f);
+  arm_planning.set_arm_z(0.717915f);
+
+  arm_planning.set_arm_roll(0.0f);
+  arm_planning.set_arm_pitch(0.0f);
+  arm_planning.set_arm_yaw(0.0f);
 
   // Initialize the message;
   proposed_joint_state.name = {"joint_1","joint_2","joint_3","joint_4","joint_5","joint_6"};
@@ -301,8 +357,10 @@ int main(int argc, char** argv)
   proposed_joint_state.position = {0.0,0.0,0.0,0.0,0.0,0.0};
 
   ros::Publisher planned_joints_pub = node_handle.advertise<sensor_msgs::JointState>("/joint_states_planned", 1000);
+  
 
   ros::Subscriber sub = node_handle.subscribe("formant_plan_move", 1000, plan_and_move);
+  ros::Subscriber home_sub = node_handle.subscribe("home", 1000, home);
 
   ros::Subscriber sub_x = node_handle.subscribe("ar3_x", 1000, new_x_value);
   ros::Subscriber sub_y = node_handle.subscribe("ar3_y", 1000, new_y_value);
@@ -320,19 +378,91 @@ int main(int argc, char** argv)
   static const std::string PLANNING_GROUP = "manipulator";
   moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
 
+  bool should_home = true;
+  bool successful_plan = false;
+  geometry_msgs::Pose last_good_pose;
+
+  double pos_thresh = 0.5f;
+  double rpy_thresh = 0.5f;
+  double pos_scaler = 0.01f;
+  double rpy_scaler = 0.01f;
+
+  // Reset values to last known good state
+  for (int i=0; i < 3; ++i) {
+    new_arm_planning.set_arm_pos(i, arm_planning.get_arm_pos(i));
+    new_arm_planning.set_arm_rpy(i, arm_planning.get_arm_rpy(i));
+
+  }
+
+
   ros::Rate rate(10);
   while (ros::ok()) {
 
 
-move_group.setEndEffectorLink("link_6");
-geometry_msgs::PoseStamped current_pose = move_group.getCurrentPose();
-      ROS_INFO_NAMED("msg", "current orientation %f %f %f %f and Position %f %f %f", current_pose.pose.orientation.w, 
-                                                                                          current_pose.pose.orientation.x, 
-                                                                                          current_pose.pose.orientation.y, 
-                                                                                          current_pose.pose.orientation.z,
-                                                                                          current_pose.pose.position.x,
-                                                                                          current_pose.pose.position.y,
-                                                                                          current_pose.pose.position.z);
+// move_group.setEndEffectorLink("link_6");
+// geometry_msgs::PoseStamped current_pose = move_group.getCurrentPose();
+//       ROS_INFO_NAMED("msg", "current orientation %f %f %f %f and Position %f %f %f", current_pose.pose.orientation.w, 
+//                                                                                           current_pose.pose.orientation.x, 
+//                                                                                           current_pose.pose.orientation.y, 
+//                                                                                           current_pose.pose.orientation.z,
+//                                                                                           current_pose.pose.position.x,
+//                                                                                           current_pose.pose.position.y,
+//                                                                                           current_pose.pose.position.z);
+
+
+  // Update the planning info with new rates
+  double new_xyz_rates[3];
+  double new_rpy_rates[3];
+  double xyz_pos[3];
+  double rpy_angles[3];
+
+
+  for (int i=0; i < 3; ++i) {
+    new_xyz_rates[i] = arm_control.get_arm_pos(i);
+    new_rpy_rates[i] = arm_control.get_arm_rpy(i);
+    xyz_pos[i] = new_arm_planning.get_arm_pos(i);
+    rpy_angles[i] = new_arm_planning.get_arm_rpy(i);
+
+
+    // ROS_INFO_NAMED("msg", "index %i data %f %f %f %f", i, xyz_pos[i], new_xyz_rates[i], rpy_angles[i], new_rpy_rates[i]);
+
+    if (abs(new_xyz_rates[i]) > pos_thresh) {
+
+      new_arm_planning.set_arm_pos(i, xyz_pos[i] + new_xyz_rates[i]*pos_scaler);
+      ROS_INFO_NAMED("msg", "Changing xyz %i to %f",i, xyz_pos[i] + new_xyz_rates[i]*pos_scaler);
+
+    }
+
+    if (abs(new_rpy_rates[i]) > rpy_thresh) {
+      new_arm_planning.set_arm_rpy(i, rpy_angles[i] + new_rpy_rates[i]*rpy_scaler);
+      // ROS_INFO_NAMED("msg", "Changing rpy %i",i);
+    }
+  }
+
+
+  // if we got a home command reset our values
+  if (arm_control.get_home()) {
+    arm_planning.set_arm_x(-0.007443f);
+    arm_planning.set_arm_y(-0.064925f);
+    arm_planning.set_arm_z(0.717915f);
+
+    arm_planning.set_arm_roll(0.0f);
+    arm_planning.set_arm_pitch(0.0f);
+    arm_planning.set_arm_yaw(0.0f);
+
+    // Reset values to home
+    for (int i=0; i < 3; ++i) {
+      new_arm_planning.set_arm_pos(i, arm_planning.get_arm_pos(i));
+      new_arm_planning.set_arm_rpy(i, arm_planning.get_arm_rpy(i));
+
+    }
+
+    arm_control.set_home(false);
+    should_home = true;
+
+  }
+
+
 
 
     geometry_msgs::Pose my_pose;
@@ -345,32 +475,32 @@ geometry_msgs::PoseStamped current_pose = move_group.getCurrentPose();
     tf2::Quaternion quat;
 
 
-    if (arm_control.get_new_move() || arm_control.get_new_plan()) {
+    if (arm_control.get_new_move() || arm_control.get_new_plan() || should_home) {
 
-      my_pose.position.x = arm_control.get_arm_x();
-      my_pose.position.y = arm_control.get_arm_y();
-      my_pose.position.z = arm_control.get_arm_z();
+      my_pose.position.x = new_arm_planning.get_arm_pos(0);
+      my_pose.position.y = new_arm_planning.get_arm_y();
+      my_pose.position.z = new_arm_planning.get_arm_z();
 
-      // yaw = atan2(my_pose.position.y, my_pose.position.x);
-      // double xp = cos(yaw)*my_pose.position.x + sin(yaw)*my_pose.position.y;
-      // pitch = atan2(my_pose.position.z, xp);
-      // roll = 0.0f;
+      quat.setRPY( new_arm_planning.get_arm_roll(), 
+                   new_arm_planning.get_arm_pitch(), 
+                   new_arm_planning.get_arm_yaw() );
 
-      quat.setRPY( arm_control.get_arm_roll(), 
-                   arm_control.get_arm_pitch(), 
-                   arm_control.get_arm_yaw() );
+      // ROS_INFO_NAMED("msg", "what %f %f %f %f",my_pose.position.x, new_arm_planning.get_arm_pos(0), new_arm_planning.get_arm_x(), 1.0);
       // quat.setRPY( 0, 0, 0 );
       quat *= static_quat;
       quat.normalize();
     }
 
 
-    if (arm_control.get_new_move()) {
+    // Plan if we need to
+    if (arm_control.get_new_plan() || should_home) {
+      
 
       my_pose.orientation.w = quat[3];
       my_pose.orientation.x = quat[0];
       my_pose.orientation.y = quat[1];
       my_pose.orientation.z = quat[2];
+
 
       ROS_INFO_NAMED("msg", "Planning for orientation %f %f %f %f and Position %f %f %f", my_pose.orientation.w, 
                                                                                           my_pose.orientation.x, 
@@ -382,7 +512,52 @@ geometry_msgs::PoseStamped current_pose = move_group.getCurrentPose();
 
       bool success = plan_pose(&move_group, my_pose, planned_joints_pub);
 
+      // We only need to capture if the plan was successful since we'll store the last good plan
       if (success) {
+        successful_plan = true;
+        last_good_pose = my_pose;
+        // update the last known good pose
+        for (int i=0; i < 3; ++i) {
+          arm_planning.set_arm_pos(i, new_arm_planning.get_arm_pos(i));
+          arm_planning.set_arm_rpy(i, new_arm_planning.get_arm_rpy(i));
+
+        }
+      } else {
+        // Reset values to last known good state
+        for (int i=0; i < 3; ++i) {
+          new_arm_planning.set_arm_pos(i, arm_planning.get_arm_pos(i));
+          new_arm_planning.set_arm_rpy(i, arm_planning.get_arm_rpy(i));
+
+        }
+      }
+
+      arm_control.set_new_plan(false);
+      if (should_home) should_home = false;
+    }
+
+
+    // Move if we need to
+    if (arm_control.get_new_move()) {
+
+      // my_pose.orientation.w = quat[3];
+      // my_pose.orientation.x = quat[0];
+      // my_pose.orientation.y = quat[1];
+      // my_pose.orientation.z = quat[2];
+
+      // ROS_INFO_NAMED("msg", "Planning for orientation %f %f %f %f and Position %f %f %f", my_pose.orientation.w, 
+      //                                                                                     my_pose.orientation.x, 
+      //                                                                                     my_pose.orientation.y, 
+      //                                                                                     my_pose.orientation.z,
+      //                                                                                     my_pose.position.x,
+      //                                                                                     my_pose.position.y,
+      //                                                                                     my_pose.position.z);
+
+      // bool success = plan_pose(&move_group, my_pose, planned_joints_pub);
+
+      // if (success) {
+      if (successful_plan) {
+        plan_pose(&move_group, last_good_pose, planned_joints_pub);
+        ROS_INFO_NAMED("msg", "Commanding Move");
         move_group.move();
         ROS_INFO_NAMED("msg", "Move complete");
       }        
@@ -390,31 +565,6 @@ geometry_msgs::PoseStamped current_pose = move_group.getCurrentPose();
       arm_control.set_new_move(false);
 
 
-    } else if (arm_control.get_new_plan()) {
-      
-
-
-    
-
-
-
-      my_pose.orientation.w = quat[3];
-      my_pose.orientation.x = quat[0];
-      my_pose.orientation.y = quat[1];
-      my_pose.orientation.z = quat[2];
-
-
-      ROS_INFO_NAMED("msg", "Planning for orientation %f %f %f %f and Position %f %f %f", my_pose.orientation.w, 
-                                                                                          my_pose.orientation.x, 
-                                                                                          my_pose.orientation.y, 
-                                                                                          my_pose.orientation.z,
-                                                                                          my_pose.position.x,
-                                                                                          my_pose.position.y,
-                                                                                          my_pose.position.z);
-
-      bool success = plan_pose(&move_group, my_pose, planned_joints_pub);
-
-      arm_control.set_new_plan(false);
     }
 
     rate.sleep();
